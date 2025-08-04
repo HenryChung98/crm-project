9970566b86e73384b96bdf7d7c1fff7cd6354152
Henry <tongsik98@gmail.com>
2025-08-04 10:58:45 -0700

    solved 'role' does not exist on type

--- src/app/(main)/(user)/profile/page.tsx
+++ src/app/(main)/(user)/profile/page.tsx
@@ -9,12 +9,8 @@ export default function ProfilePage() {
       <h1 className="text-4xl font-bold mb-8">User Profile</h1>
       <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
         <div className="mb-4">
-          <label
-            htmlFor="username"
-            className="block text-gray-700 text-sm font-bold mb-2"
-          >
-            {user?.first_name}, {user?.last_name}, {user?.email}, {user?.role},
-            {user?.image},{" "}
+          <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">
+            {user?.first_name}, {user?.last_name}, {user?.email},{user?.image}
             {user?.created_at && new Date(user.created_at).toLocaleString()}
           </label>
         </div>
